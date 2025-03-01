7bbf445c5882b91c3907af8de68deb2c8edf0a47
cds.dev010 <cds.dev010@gmail.com>
2025-03-01 13:47:43 +0700

    feat: add task 6
    apply response code

--- src/routes/eventRoute.ts
+++ src/routes/eventRoute.ts
@@ -11,6 +11,11 @@ router.get("/", async (req, res) => {
         const keyword = req.query.keyword as string;
         const result = await service.getAllEventsWithPagination(keyword, pageSize, pageNo);
 
+        if (result.events.length === 0) {
+            res.status(404).send("No event found");
+            return;
+        }
+
         res.setHeader("x-total-count", result.count.toString());
         res.json(result.events);
     } else if (req.query.category) {
