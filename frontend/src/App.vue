118c4b20d61d86af2471fb339da4ef771a603349
tyleeer <127297167+tyleeer@users.noreply.github.com>
2025-03-08 13:39:41 +0700

    add(lab06): task 5 ทดสอบลำดับขั้นของ component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,12 +7,11 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Event</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
     </header>
-
     <RouterView />
   </div>
 </template>
--- frontend/src/views/EventListView.vue
+++ frontend/src/views/EventListView.vue
@@ -46,12 +46,13 @@ const events = ref([
 </script>
 
 <template>
+  <!-- <div v-if="events.length === 0">No events</div>
+  <div v-else-if="events.length === 1">Only one event</div>
+  <div v-else>
+    <EventCard v-for="event in events" :key="event.id" />
+  </div> -->
+  <h1>Events For Good</h1>
   <div class="events">
-    <!-- <div v-if="events.length === 0">No events</div>
-    <div v-else-if="events.length === 1">Only one event</div>
-    <div v-else>
-      <EventCard v-for="event in events" :key="event.id" />
-    </div> -->
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
