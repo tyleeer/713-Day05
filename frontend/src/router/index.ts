9bdf8033c387ecd3cc226e3282745de1c546cf4e
tyleeer <127297167+tyleeer@users.noreply.github.com>
2025-03-08 15:29:51 +0700

    add(lab06): task 7 การแสดงข้อมูลเฉพาะสิ่งของและการใช้งาน router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/event/12',
+      name: 'event-detail-views',
+      component: import('../views/EventDetailView.vue'),
+    }
   ],
 })
 
---
+++ frontend/src/views/EventDetailView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types'
+import eventService from '@/services/EventService'
+const event = ref<Event>()
+const id = ref<number>(1)
+
+eventService
+      .getEvent(id.value)
+      .then((response) => {
+            event.value = response.data
+              })
+      .catch((error) => {
+            console.error('There was an error!', error)
+              })
+</script>
+
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+    <div v-else>
+        <p>Loading...</p>
+    </div>
+</template>
