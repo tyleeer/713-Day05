19fdc202eee028baf9fc8b6b4265bea3138a04b1
tyleeer <127297167+tyleeer@users.noreply.github.com>
2025-03-15 14:09:22 +0700

    chore: update backend url

--- frontend/src/services/AxiosService.ts
+++ frontend/src/services/AxiosService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+const backendUrl = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_API_URL;
 const apiClient = axios.create({
     baseURL: backendUrl,
     withCredentials: false,
